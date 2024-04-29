bae36c64e39613d8aa2cfd628b1ffb1f2e67122d
streamify-cnit195 <163710500+streamify-cnit195@users.noreply.github.com>
2024-04-28 18:29:32 -0700

    Update Navbar.jsx
    Get rid of hover effect so the "STREAMIFY" logo doesn't appear as a link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 z-[100] absolute w-full">
-      <h1 className="text-red-600 text-4xl font-bold cursor-pointer">STREAMIFY</h1>
+      <h1 className="text-red-600 text-4xl font-bold ">STREAMIFY</h1>
       <div>
         <button className="text-white pr-4">Sign in </button>
         <button className=" bg-red-600 px-6 py-2 rounded text-white">Sign up</button>
