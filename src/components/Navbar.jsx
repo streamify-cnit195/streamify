580840c72e8194b8c66ec26a58bd3ce71c29ff44
streamify-cnit195 <163710500+streamify-cnit195@users.noreply.github.com>
2024-04-29 20:04:56 -0700

    Add hover effect to sign in & sign up buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,10 @@ const Navbar = () => {
     <div className="flex items-center justify-between p-4 z-[100] absolute w-full">
       <h1 className="text-red-600 text-4xl font-bold ">STREAMIFY</h1>
       <div>
-        <button className="text-white pr-4">Sign in </button>
-        <button className=" bg-red-600 px-6 py-2 rounded text-white">Sign up</button>
+         <button className="text-white pr-4 ">Sign in </button>
+        <button className=" bg-red-600 hover:bg-red-600/90 pr-4 px-3 py-1 md:px-5 md:py-2 lg:px-6  rounded text-white">
+          Sign up
+        </button>
       </div>
     </div>
   );
