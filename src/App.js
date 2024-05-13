5dca2e6d54f120caf3c238a676f7b88df748c20a
Therese Chang <56442073+erryte@users.noreply.github.com>
2024-05-12 23:38:15 -0700

    Update App.js
    swapped out Navbar with Navbarhook
    added components for future dev

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
+import NavbarHook from "./components/NavbarHook";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Navbar />
+      // <Navbar />
+      <NavbarHook />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/" element={<Contact />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Signup />} />
       </Routes>
+      <Footer />
     </>
   );
 }
 
 export default App;
+
